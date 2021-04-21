daa663f1f4f839f73a6714a4e50da4caa6b69b5b
mvieira4 <47149053+mvieira4@users.noreply.github.com>
2021-04-21 18:16:45 -0400

    Got multiple connection working

--- Python/p2p_chat_session_v2.py
+++ Python/p2p_chat_session_v2.py
@@ -1,5 +1,4 @@
 from socket import *
-from p2p_v2 import p2p_connection
 from threading import *
 
 
@@ -19,54 +18,61 @@ class p2p_chat_session:
     # Starts session
     def __init__(self, cent_port):
         # Creates central socket
-        self.cent_addr = (gethostname(),cent_port)
+        self.cent_addr = (gethostname(), cent_port)
         self.cent_sock = socket(AF_INET, SOCK_STREAM)
         self.cent_sock.bind(self.cent_addr)
-        self.cent_sock.listen(2)
+        self.cent_sock.listen()
+        print(f"[i] Created Central Socket At {gethostname()}:{cent_port}")
 
         # Starts waiting for inbound connections
-        wait = Thread(target = self.acpt_p2p)
+        wait = Thread(target=self.acpt_p2p, daemon=True)
         wait.start()
+        print("[*] Waiting For Inbound Connection...")
 
         # Starts waiting for string to send to all connections
-        send_all = Thread(target = self.send_all_str)
+        send_all = Thread(target=self.send_all_str, daemon=True)
         send_all.start()
+
+        print("[i] Session Started")
 
     # Requests p2p connection
     def requ_p2p(self, target_ip, target_port):
         # Creates send socket
         send_pipe = socket(AF_INET, SOCK_STREAM)
-        send_pipe.connect((target_ip,target_port))
+        send_pipe.connect((target_ip, target_port))
+        print(f"[i] Created Sending Pipeline")
 
-        # Gets central socket info, sends it, and accepts connection
-        info = " ".join(self.cent_addr)
-        send_pipe.send(info.encode("utf-8"))
-        recv_pipe, _ = self.cent_sock.accept()
+        recv_pipe = socket(AF_INET, SOCK_STREAM)
+        recv_pipe.connect((target_ip, target_port))
+        print(f"[i] Created Receiving Pipeline")
 
         # Starts listening for incoming messages and adds them to message list
-        recv = Thread(target = self.recv_str, args = (recv_pipe,))
+        recv = Thread(target=self.recv_str, args=(recv_pipe,))
         recv.start()
+        print("[i] Listening On Receiving Pipeline")
 
         # Adds receiving socket and send socket as tuple to connection list
         self.connection_list.append((recv_pipe, send_pipe))
-    
+        print(f"[+] Connected To {len(self.connection_list)} Connections")
+
     # Accepts p2p connection
     def acpt_p2p(self):
         # Accepts connections as long as session active
-        while(self.acpt_p2p == True):
+        while(self.active == True):
             recv_pipe, _ = self.cent_sock.accept()
-            
-            # Receives peer central socket and connects
-            target_ip, target_port = recv_pipe.recv(1024)
-            send_pipe = socket(AF_INET, SOCK_STREAM)
-            send_pipe.connect((target_ip,target_port))
+            print("[i] Created Receiving Pipeline")
+
+            send_pipe, _ = self.cent_sock.accept()
+            print(f"[i] Created Sending Pipeline")
 
             # Starts listening for incoming messages and adds them to message list
-            recv = Thread(target = self.recv_str, args = (recv_pipe,))
+            recv = Thread(target=self.recv_str, args=(recv_pipe,),daemon=True)
             recv.start()
+            print("[i] Listening On Receiving Pipeline")
 
             # Adds receiving socket and send socket as tuple to connection list
             self.connection_list.append((recv_pipe, send_pipe))
+            print(f"[+] Connected To {len(self.connection_list)} Connections")
 
     # Print all messages
     def recv_prt_all(self):
@@ -82,17 +88,16 @@ class p2p_chat_session:
             while(self.send_queue != [] and self.connection_list != []):
                 # Takes first item in list and sends it to all connections
                 string = self.send_queue.pop()
-                self.message_list.append(string)
                 for _, send_pipe in self.connection_list:
                     send_pipe.send(string.encode("utf-8"))
 
     # Puts string in send queue
-    def send_str(self,string):
+    def send_str(self, string):
         self.send_queue.append(string)
 
     # Recives string from peer and ads it to message list
     def recv_str(self, recv_pipe) -> str:
-        while(self.active == True):    
+        while(self.active == True):
             self.message_list.append(recv_pipe.recv(1024).decode("utf-8"))
 
     # Sets session to inactive and closes all sockets
--- Python/ui_v3.py
+++ Python/ui_v3.py
@@ -1,10 +1,14 @@
 #from tkinter import *
 import tkinter as tk
+from socket import *
+from p2p_chat_session_v2 import *
 
+cent_port = input("[?] Enter Central Port Number: ")
 root = tk.Tk()
 root.geometry("600x600")  # SIZE OF APP WINDOW
 root.title("CHAT APP")    # TITLE OF APP WINDOW
-root.configure(bg="black")  # changes the background color
+root.configure(bg="white")  # changes the background color
+session = p2p_chat_session(int(cent_port))
 
 
 # lbl = Label(root, text="Hello")
@@ -31,14 +35,21 @@ root.configure(bg="black")  # changes the background color
 #T1 = Text((root) ,height = 18, width=100)
 #T1.pack(side = TOP)
 
+def disp_msg(session):
+    while(session.active == True):
+      while(session.message_list != []):
+        T1.insert(tk.INSERT, session.message_list.pop())
 
 def clicked():                                                   # clicked function defined here
   # T1 = tk.Text((root) ,height = 18, width=100)
     #T1.pack(side = tk.TOP)
     # should retrieve the text entered by user
     retrieveinput = T.get("1.0", tk.END)
+    T1.insert(tk.INSERT, retrieveinput)
+
     # clears the text entry box after each sent message
   # print(T.get("1.0",'end-1c'))
+    session.send_str(retrieveinput)
 
 
 def connect():  # this is the function that will execute when the connect button is pressed
@@ -48,30 +59,31 @@ def connect():  # this is the function that will execute when the connect button
     IPValue = IPBox.get("1.0", "end-1c")  # STORES THE IP VALUE
     PORTValue = PortBox.get("1.0", "end-1c")  # STORES THE PORT VALUE
 
-    retrieveinput2 = IPBox.get("1.0", tk.END)
+    retrieveinput2 = PortBox.get("1.0", tk.END)
     T1.insert(tk.INSERT, retrieveinput2)
     T.delete("1.0", tk.END)
 
+    session.requ_p2p(gethostname(), int(PORTValue))
+
     # this is the function that will execute when the disconnect button is pressed
-
-
 def disconnect():
     # DELETE EVERYTHING WITHIN THIS FUNCTION AND ADD THE CODE TO DISCONNECT
     retrieveinput = T.get("1.0", tk.END)
     T1.insert(tk.INSERT, retrieveinput)
     T.delete("1.0", tk.END)
+    session.close_ses()
 
 
-frame = tk.Frame(root, bg="black")
+frame = tk.Frame(root, bg="white")
 frame.pack(side=tk.LEFT, anchor=tk.NW)
 
-frame1 = tk.Frame(root, bg="black")
+frame1 = tk.Frame(root, bg="white")
 frame1.pack(side=tk.LEFT, anchor=tk.N)
 
 
 # BUTTON DEFINITIONS BELOW
 
-btn = tk.Button(root, text=" Send ", fg="green", bg="black", command=clicked)
+btn = tk.Button(root, text=" Send ", fg="green", bg="white", command=clicked)
 btn.pack(side=tk.RIGHT, anchor=tk.SW)
 #btn.pack(side = tk.RIGHT, fill = tk.Y)
 #btn.bind("<Return>", clicked)
@@ -83,19 +95,19 @@ PortBox = tk.Text(frame1, height=1, width=10)
 PortBox.pack(side=tk.TOP, anchor=tk.N)
 
 disconnectbtn = tk.Button(frame1, text="  Disconnect    ",
-                          fg="red", bg="black", command=disconnect)
+                          fg="red", bg="white", command=disconnect)
 disconnectbtn.pack(side=tk.BOTTOM)
 #disconnectbtn.pack(side = tk.TOP, fill = tk.Y)
 
 connectbtn = tk.Button(frame1, text="     Connect     ",
-                       fg="blue", bg="black", command=connect)
+                       fg="blue", bg="white", command=connect)
 connectbtn.pack(side=tk.BOTTOM)
 #connectbtn.pack(side = tk.TOP, fill = tk.Y)
 
-IPlabel = tk.Label(frame, text="IP", fg="green", bg="black")
+IPlabel = tk.Label(frame, text="IP", fg="green", bg="white")
 IPlabel.pack(side=tk.TOP, anchor=tk.NW)
 
-portlabel = tk.Label(frame, text="PORT", fg="green", bg="black")
+portlabel = tk.Label(frame, text="PORT", fg="green", bg="white")
 portlabel.pack(side=tk.BOTTOM, anchor=tk.NW)
 
 
@@ -107,5 +119,7 @@ T1.pack(side=tk.TOP)
 T = tk.Text((root), height=4, width=100)
 T.pack(side=tk.BOTTOM)
 
+disp = Thread(target=disp_msg,args=(session,),daemon=True)
+disp.start()
 
 root.mainloop()
