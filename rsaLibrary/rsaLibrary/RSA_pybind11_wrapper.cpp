3825b4a409c180c3de508e7c38dac3d51641e714
Mycroft0Holmes <agabriel2@umassd.edu>
2021-04-27 11:29:44 -0400

    Added bindings

--- rsaLibrary/rsaLibrary/RSA_pybind11_wrapper.cpp
+++ rsaLibrary/rsaLibrary/RSA_pybind11_wrapper.cpp
@@ -1,15 +1,18 @@
 #include <stdio.h>
-#include <pybind11/pybind11.h>
+#include "pybind11.h"
 #include "rsaLibrary.h"
 
+namespace rsaLibrary {
 PYBIND11_MODULE(rsaBind, m) {
-	m.def("gcd", &gcd, "A function that finds the greatest common denomenator");
-	m.def("RSA", &RSA, "A function that generates an rsa key and encyrpts/decrypts raw ascii data");
-	m.def("toAscii", &toAscii, "A function that converts a char input to an ascii double");
-	m.def("toChar", &toChar, "A function that converts a char input to an ascii double");
+	m.def("gcd", &rsaClass::gcd, "A function that finds the greatest common denomenator");
+	m.def("RSA", &rsaClass::RSA, "A function that generates an rsa key and encyrpts/decrypts raw ascii data");
+	m.def("toAscii", &rsaClass::toAscii, "A function that converts a char input to an ascii double");
+	m.def("toChar", &rsaClass::toChar, "A function that converts a char input to an ascii double");
+}
 }
 
 //try the following (or something similar) if the above does not work
+
 /*
 PYBIND11_MODULE(rsaBind, n) {
 	n.def("gcd", &gcd, "A function that finds the greatest common denomenator");
